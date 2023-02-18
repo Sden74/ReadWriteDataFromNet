783013abb628303d30e07f18de7abf31f7c52271
Denis Sitnov <sitnov.den74@gmail.com>
2023-02-18 11:15:43 +0300

    Как теперь исправить ситуацию и чтобы наша программа не вылетала? Простой проверкой ну конечно же.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         MyData data = getMyData();
-        print(data.toString());
+        if(data==null){
+            print("object was null");
+        }else{
+            print(data.toString());
+        }
+
+
     }
 
     private static void print(String string) {
