17d664e2f587e8d0e0c067a437826920594e49d2
Denis Sitnov <sitnov.den74@gmail.com>
2023-02-18 11:10:32 +0300

    в переменной data хранится null. А нул это не обьект и у него нельзя вызвать метод toString(). Именно поэтому мы получаем исключение и наша программа заканчивается с кодом 1. Т.е. не успешно.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyData data = getMyData();
+        print(data.toString());
+    }
+
+    private static void print(String string) {
+        System.out.println(string);
+    }
+
+    private static MyData getMyData() {
+        return null;
     }
 }
