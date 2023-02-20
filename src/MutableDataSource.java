0e685670cb00040ac7a2814d65ee6754425f5627
Denis Sitnov <sitnov.den74@gmail.com>
2023-02-20 16:07:21 +0300

    метод MyData getData() встречается в 2 разных интерфейсах, но как мы можем исправить это? И здесь мы будем использовать интерфейсы на полную катушку. Одна из самых классных фишек языка Джава в том, что можно наследовать интефейсы.
    public interface MutableDataSource extends DataSource {
        void saveData(@NotNull MyData data);
    }

--- src/MutableDataSource.java
+++ src/MutableDataSource.java
@@ -1,8 +1,6 @@
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public interface MutableDataSource {
-    @Nullable MyData getData();
-
+public interface MutableDataSource extends DataSource {
     void saveData(@NotNull MyData data);
 }
